03f90a0c4dc55c0e995f399cf0c1f2fad3b366b4
Ilya Chikmarev <ichikmarev@gmail.com>
2019-04-21 21:19:34 +0300

    Keyword fix

--- include/keyword_init_table.h
+++ include/keyword_init_table.h
@@ -11,7 +11,7 @@
 #define KEYWORD_INIT_TABLE_H
 #   include <cstddef>
 #   include "../include/state_for_char.h"
-namespace lynx_scanner{
+namespace mur128m_scanner{
     extern const State_for_char keyword_init_table[];
     extern const std::size_t    keyword_init_table_size;
 };
--- src/keyword_init_table.cpp
+++ src/keyword_init_table.cpp
@@ -10,14 +10,13 @@
 #include "../include/keyword_init_table.h"
 #include "../include/mysize.h"
 
-namespace lynx_scanner{
+namespace mur128m_scanner{
     const State_for_char keyword_init_table[] = {
-        {0,   U'б'}, {20,  U'в'}, {56,  U'г'},
-        {64,  U'д'}, {67,  U'е'}, {71,  U'и'},
-        {83,  U'к'}, {100, U'л'}, {112, U'м'},
-        {130, U'н'}, {135, U'о'}, {147, U'п'},
-        {172, U'р'}, {178, U'с'}, {198, U'т'},
-        {202, U'ф'}, {209, U'ц'}, {221, U'ч'}
+        {0,   U'a'}, {20,  U'b'}, {56,  U'c'},
+        {64,  U'd'}, {67,  U'f'}, {71,  U'l'},
+        {83,  U'm'}, {100, U'n'}, {112, U'o'},
+        {130, U'p'}, {135, U'r'}, {147, U's'},
+        {172, U't'}, {178, U'u'}, {198, U'x'}
     };
 
     const std::size_t    keyword_init_table_size = size(keyword_init_table);
