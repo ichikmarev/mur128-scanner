ccc2a495d3798f317e8a5cab8a5e377d44dd4da3
Parallels <parallels@debian-gnu-linux-vm.localdomain>
2019-06-16 20:55:25 +0300

    Fixes

--- src/keyword_init_table.cpp
+++ src/keyword_init_table.cpp
@@ -9,12 +9,12 @@
 namespace mur128m_scanner{
     const State_for_char keyword_init_table[] = {
         {2,   U'a'}, {0,   U'b'}, {13,  U'c'},
-        {59,  U'd'}, {72,  U'f'}, {116, U'i'},
-        {97,  U'j'}, {148, U'l'}, {196, U'm'},
-        {155, U'n'}, {141, U'o'}, {216, U'p'},
-        {162, U'r'}, {204, U's'}, {158, U't'},
-        {128, U'u'}, {144, U'x'}, {222, U'e'},
-	{232, U'w'}
+        {59,  U'd'}, {72,  U'f'}, {148, U'i'},
+        {129, U'j'}, {180, U'l'}, {196, U'm'},
+        {187, U'n'}, {173, U'o'}, {281, U'p'},
+        {194, U'r'}, {268, U's'}, {190, U't'},
+        {160, U'u'}, {176, U'x'}, {287, U'e'},
+	{297, U'w'}
     };
 
     const std::size_t    keyword_init_table_size = size(keyword_init_table);
